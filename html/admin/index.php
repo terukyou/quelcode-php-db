a724665c6482046b7c9774f458995a3f28030498
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-18 14:03:32 +0900

    index.phpのhtml追加

---
+++ html/admin/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link rel="stylesheet" href="..\entry\style.css">
+    <title>トップ</title>
+</head>
+
+<body>
+<main class="flex">
+    <header>
+        <a href="#">トップ</a>
+        <a href="./data/list.php">申し込み</a>
+    </header>
+    <section>
+        <h2>トップページ</h2>
+        <table border="">
+            <div class="states">
+                <tr>
+                    申し込み件数
+                    <td>ステータス</td>
+                    <td>件数</td>
+                </tr>
+                <tr>
+                    <td>合計</td>
+                    <td>
+                    </td>
+                </tr>
+            </div>
+        </table>
+    </section>
+</main>
+</body>
+
+</html>
