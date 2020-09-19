6b65526f6d5cae0a2cc39edae44e90f4f3d25408
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-19 15:34:31 +0900

    申し込み詳細画面のhtml追加

---
+++ html/admin/data/detail.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link rel="stylesheet" href="..\..\entry\style.css">
+    <title>申し込み詳細</title>
+</head>
+
+<body>
+    <main class="flex">
+        <header>
+            <a href="..\index.php">トップ</a>
+            <a href="./list.php">申し込み</a>
+        </header>
+        <section>
+            <h2>申し込み詳細</h2>
+            </select>
+            <p>No</p>
+            <p>申込日時</p>
+            <p>名前</p>
+            <p>ふりがな</p>
+            <p>メールアドレス</p>
+            <p>電話番号</p>
+            <p>生年月日</p>
+            <p>都道府県</p>
+            <a href="list.php">一覧戻る</a>
+        </section>
+    </main>
+</body>
+
+</html>
