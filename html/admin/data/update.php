7912ced966ec9f5d1d09445e3f0c1ab3f5490f08
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-23 16:42:14 +0900

    申し込み詳細更新画面のステータスのupdate機能追加

---
+++ html/admin/data/update.php
@@ -0,0 +1,5 @@
+<?php require('../../entry/dbconnect.php');
+$updates=$db->prepare('UPDATE users SET status_id=? WHERE id=?');
+$updates->bindValue(1,$_POST['status'],PDO::PARAM_INT);
+$updates->bindValue(2,$_POST['id'],PDO::PARAM_INT);
+$updates->execute();
