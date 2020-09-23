60499257a2d35cb27ac2c02a7bddcd933bdee827
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-09-23 16:44:02 +0900

    元のURLにリダイレクトして戻る機能追加

--- html/admin/data/update.php
+++ html/admin/data/update.php
@@ -3,3 +3,5 @@ $updates=$db->prepare('UPDATE users SET status_id=? WHERE id=?');
 $updates->bindValue(1,$_POST['status'],PDO::PARAM_INT);
 $updates->bindValue(2,$_POST['id'],PDO::PARAM_INT);
 $updates->execute();
+header('Location:detail.php?id='.$_POST['id']);
+exit();
